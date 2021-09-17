f83364dbba1717d6a8ec10765290b93339f26d62
isyoo1213 <moethns@gmail.com>
2021-09-17 20:58:44 +0900

    feat: Create java file for Proxy Pattern

---
+++ spring/proxy/Browser.java
@@ -0,0 +1,18 @@
+package proxy;
+
+public class Browser implements IBrowser {
+
+    private String url;
+
+    public Browser(String url){
+        this.url = url;
+    }
+
+
+    @Override
+    public Html show() {
+        System.out.println("browser loading html : " + url);
+        return new Html(url);
+    }
+}
+
