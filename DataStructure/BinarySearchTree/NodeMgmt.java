38a06b73a74490573236bf64342e28233f72cee1
isyoo1213 <moethns@gmail.com>
2021-10-12 20:33:13 +0900

    feat: add Nodeinsert method

--- DataStructure/BinarySearchTree/NodeMgmt.java
+++ DataStructure/BinarySearchTree/NodeMgmt.java
@@ -14,4 +14,33 @@ public class NodeMgmt {
         }
     }
 
+     public boolean insertNode(int data){
+        //CASE1: Node가 하나도 없을 때
+        if(this.head == null){
+            this.head = new Node(data); // root node 생성
+        } else { //CASE2: Node 1개 이상 있을 때
+            Node findNode = this.head;
+            while(true) {
+                //CASE2-1: 현재 Node의 왼쪽에 Node가 들어가야 할 때
+                if (data < findNode.value) {
+                    if (findNode.left != null) {
+                        findNode = findNode.left;
+                    } else {
+                        findNode.left = new Node(data);
+                        break;
+                    }
+
+                } else {//CASE2-2: 현재 Node의 오른쪽에 Node가 들어가야 할 때
+                    if(findNode.right != null){
+                        findNode = findNode.right;
+                    } else {
+                        findNode.right = new Node(data);
+                        break;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
 }
